bfcf17ee2e05d9bd6ac06e91cacd65d51e105a89
Anuradha Weeraman <aweeraman@gmail.com>
2021-04-18 17:55:06 +0530

    Changed mutt cmd

--- config.h
+++ config.h
@@ -65,7 +65,6 @@ static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufon
 static const char *termcmd[]    = { "st", NULL };
 static const char *lockcmd[]    = { "slock", NULL };
 static const char *browsercmd[] = { "chromium", NULL };
-static const char *mailcmd[] = { "st -e mutt", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -107,7 +106,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY,                       XK_s,      spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },
-	{ MODKEY,                       XK_e,      spawn,          {.v = mailcmd } },
+	{ MODKEY,                       XK_e,      spawn,          CMD("st -e mutt") },
 };
 
 /* button definitions */
