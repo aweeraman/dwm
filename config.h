579dca2c0ce68ce482135cd5a175e552f6ec8098
Anuradha Weeraman <aweeraman@gmail.com>
2021-04-18 17:10:41 +0530

    Shortcut for mutt

--- config.h
+++ config.h
@@ -61,6 +61,7 @@ static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufon
 static const char *termcmd[]    = { "st", NULL };
 static const char *lockcmd[]    = { "slock", NULL };
 static const char *browsercmd[] = { "chromium", NULL };
+static const char *mailcmd[] = { "mutt", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -99,6 +100,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY,                       XK_s,      spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = mailcmd } },
 };
 
 /* button definitions */
